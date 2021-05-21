5b130da870d58d9d8382ba3745352da4b0758375
Marc Schubhan <marc.schubhan@centigrade.de>
2021-05-21 15:09:19 +0200

    revert class ComponentType to enum

--- src/model/components/types/ComponentType.ts
+++ src/model/components/types/ComponentType.ts
@@ -1,12 +1,12 @@
-export class ComponentType {
-  public static ALL = "UNDEFINED";
-  public static PERSONA = "PERSONA";
-  public static POINTER = "POINTER";
-  public static TAG = "TAG";
-  public static STORY = "STORY";
-  public static USER = "USER";
-  public static INPUT_ACTION = "INPUT_ACTION";
-  public static INPUT_ACTION_MAP = "INPUT_ACTION_MAP";
-  public static USER_INTERFACE = "UI";
-  public static DESCRIPTION = "DESCRIPTION";
+export enum ComponentType {
+  ALL = "UNDEFINED",
+  PERSONA = "PERSONA",
+  POINTER = "POINTER",
+  TAG = "TAG",
+  STORY = "STORY",
+  USER = "USER",
+  INPUT_ACTION = "INPUT_ACTION",
+  INPUT_ACTION_MAP = "INPUT_ACTION_MAP",
+  USER_INTERFACE = "UI",
+  DESCRIPTION = "DESCRIPTION",
 }
